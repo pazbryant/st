c3116869c65a9307b2c45d2041dd8297f9e6bc69
Bryant Paz <pazbryant7@outlook.com>
2024-07-15 19:12:57 -0600

    chore: update font config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Maple Mono NF:pixelsize=12:style:Bold:antialias=true:autohint=true";
-static char *font2[] = { "monospace:pixelsize=12:antialias=true:autohint=true" };
+static char *font = "Maple Mono NF:pixelsize=12:style:Regular:antialias=true:autohint=true";
+static char *font2[] = { "JetBrainsMono NFM:pixelsize=12:antialias=true:autohint=true" };
+static char *font3[] = { "monospace:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
@@ -25,7 +26,7 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
+static float cwscale = 1.1;
 static float chscale = 1.0;
 
 /*
