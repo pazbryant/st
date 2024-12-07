f40667a0ed0afa478057e898066e22ab31e0a182
Bryant Paz <pazbryant@proton.me>
2024-12-07 11:35:12 -0600

    chore: cursor shape from underscore to block.
    - update .gitignore file.

--- config.def.h
+++ config.def.h
@@ -167,7 +167,7 @@ static unsigned int defaultrcs = 258;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 4;
+static unsigned int cursorshape = 2;
 
 /*
  * Default columns and rows numbers
