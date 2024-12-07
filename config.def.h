a4428c2bfa7677db811ebd00c3d565800a8fb670
Bryant Paz <pazbryant@proton.me>
2024-12-06 18:50:20 -0600

    chore: change cursor shape

--- config.def.h
+++ config.def.h
@@ -167,7 +167,7 @@ unsigned int defaultrcs = 258;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 4;
+static unsigned int cursorshape = 2;
 
 /*
  * Default columns and rows numbers
