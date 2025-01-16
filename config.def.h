2e0a302371b16dd9d782c2db6528baf29dff8979
Bryant Paz <pazbryant@proton.me>
2025-01-15 20:37:11 -0600

    chore(borderpx): set borderpx default to 0

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static char *font2[] = {
   "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
 };
 
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
